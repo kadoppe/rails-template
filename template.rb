ad3aee30c9971e0f31a0d888bb3c1fdeb7afd8d8
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-18 12:03:27 +0900

    Remove guard

--- template.rb
+++ template.rb
@@ -14,16 +14,12 @@ gem_group :development do
   gem 'pry-rails'
   gem 'request_profiler'
   gem 'simplecov'
-  gem 'spring'
 end
 
 gem_group :development, :test do
   gem 'factory_girl_rails'
-  gem 'guard'
-  gem 'guard-rspec'
   gem 'rspec-rails'
   gem 'spring'
-  gem 'terminal-notifier-guard'
 end
 
 run_bundle
@@ -38,13 +34,3 @@ git commit: "-m 'Initial commit'"
 generate 'rspec:install'
 git add: '.'
 git commit: "-m 'Install rspec'"
-
-# guard
-run 'bundle exec guard init rspec'
-gsub_file 'Guardfile', 'guard :rspec do', 'guard :rspec, spring: true do'
-git add: '.'
-git commit: "-m 'Install guard'"
-
-
-
-
